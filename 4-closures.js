07a994ae002f168fbe20e2c9377b1b2ba1fd1a33
Stefano Fadda <s.fadda@lynx4you.it>
2015-09-22 17:15:13 +0200

    added my solutions

---
+++ 4-closures.js
@@ -0,0 +1,20 @@
+/*
+# Your Mission
+
+Modify your solution from the previous lesson to set bar = true inside zip(),
+then return the function zip as the result of foo()
+
+Once complete, execute scope-chains-closures verify <your-file.js> to verify your
+solution.
+*/
+function foo(){
+	var bar;
+	quux = 0;
+
+	function zip(){
+		var quux = 1;
+		bar = true;
+	};
+
+	return zip;
+}
